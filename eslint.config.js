475a1341b8d0fa19089b54f9a188daca6ac2aa96
yk-graph <yokokura.tatsuya@gmail.com>
2026-07-24 13:50:56 -0700

    chore: set up ESLint flat config with TS 6 for typescript-eslint compatibility

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+  },
+  prettier, // ignore prettier rules that conflict with eslint rules
+)
